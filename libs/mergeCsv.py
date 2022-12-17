c8b8ac0eda3d13126b7785c6f2ad716fe9407d19
reiji-9720 <shino327gh@gmail.com>
2022-12-18 04:04:20 +0900

    データフォーマットの設定
    cellごとにデータフォーマットの型式を定めた
    1. 日付(yyyy-mm-dd)
    2. 時間(hh:mm:ss.0000)
    3. ミリ秒(#,##0)
    4~6. 加速度(0.000000)

--- libs/mergeCsv.py
+++ libs/mergeCsv.py
@@ -17,7 +17,7 @@ def merge(csv_filename,i,Path,wb):
     with open(csv_filename,newline='') as csvf:
         # reading a csv file
         data = csv.reader(csvf)
-        
+
         # Getting a new sheet
         wb.create_sheet(index = i, title=csv_filename_only)
         sheet = wb[csv_filename_only]
@@ -30,3 +30,29 @@ def merge(csv_filename,i,Path,wb):
                 sheet.cell(row = r,column = c).value = v
                 c += 1
             r += 1 # row
+
+        # columnのwidth設定
+        # column_width = {'B': 3.5, 'C': 15, 'D': 20, 'E': 10, 'F': 10, 'G': 10}
+        # for col, width in column_width.items():
+        #     sheet.column_dimensions[col].width = width
+
+
+        # セルのデータフォーマット指定（デバッグではうまくいくがエクセル側で問題がおきる）
+        for row in sheet.iter_rows(min_col=1, max_col=6, min_row=2):
+            row[0].number_format = 'yyyy-mm-dd'
+            row[1].number_format = 'hh:mm:ss.0000'
+            row[2].number_format = '#,##0'
+            row[3].number_format = '0.000000'
+            row[4].number_format = '0.000000'
+            row[5].number_format = '0.000000'
+
+
+        # sheet.cell(row=2,column=3).number_format = "#,##0"
+        # sheet.cell(row=100,column=3).number_format = "#,##0"
+        # sheet.cell(row=100,column=4).number_format = "0.000000"
+        print(sheet.cell(row=2,column=1).number_format)
+        print(sheet.cell(row=2,column=2).number_format)
+        print(sheet.cell(row=2,column=3).number_format)
+        print(sheet.cell(row=2,column=4).number_format)
+        print(sheet.cell(row=2,column=5).number_format)
+        print(sheet.cell(row=2,column=6).number_format)
