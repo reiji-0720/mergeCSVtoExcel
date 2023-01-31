5a7d6d205cf8f4dcefd66686fdc5df252b9df161
reiji-0720 <shino327gh@gmail.com>
2023-01-31 19:23:10 +0900

    データ参照フォルダの作成

--- convertion.py
+++ convertion.py
@@ -1,10 +1,10 @@
 from libs import foundCsv
 
 # 作成するエクセルのファイル名
-excel_filename = './excel_test.xlsx'
+excel_filename = './excel_data1.xlsx'
 
 # csvデータがあるところまでのPATH
-Path = './sampledata/'
+Path = './data1/'
 
 foundCsv.simple_check(Path,excel_filename)
 
